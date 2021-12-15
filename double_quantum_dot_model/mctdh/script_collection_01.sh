7768b7d54eb85ab54eafc6395ab197bb341505de
fedepont <pont.federico@unc.edu.ar>
2021-12-15 08:42:47 -0300

    Esta listo el script de las energias

--- double_quantum_dot_model/mctdh/script_collection_01.sh
+++ double_quantum_dot_model/mctdh/script_collection_01.sh
@@ -5,20 +5,20 @@ index_min=0.4
 index_max=1.1
 delta_index=0.1
 PWD=$(pwd)
-echo $PWD
+#echo $PWD
 # +++ Imprimo mensaje
-echo #
-echo ++++++++++++++++++ START ++++++++++++++++++
-echo #
+#echo #
+#echo ++++++++++++++++++ START ++++++++++++++++++
+#echo #
 # +++ Bucle for para crear un directorio diferente para cada valor del parámetro V_L
 ## The C-style Bash for loop ##
 #for i in seq -f "%f" $index_min $delta_index $index_max
 for i in $(seq $index_min $delta_index $index_max);do
-  echo $i
+  #echo $i
   cd results-V_L-$i
   #for q in 1..2
     energ=($(awk 'NR==5,NR==25 {print $3}' veigen_X1))
   #done
-  echo ${energ[2]}
+  echo ${i} ${energ[@]}
   cd ..
 done
