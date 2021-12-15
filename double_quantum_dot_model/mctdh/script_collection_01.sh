819c4c2c0ed6cc0e466ba6630da9fd2627d58cb1
mendzmartin <chmartinmendez@gmail.com>
2021-12-15 10:58:27 -0300

    15/12/2021-10:58h

--- double_quantum_dot_model/mctdh/script_collection_01.sh
+++ double_quantum_dot_model/mctdh/script_collection_01.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 
-# +++ Declaración de parámetros
+#echo +++++++++++++++++++++++++++++++++++++++++++++++++
+#echo +++			parameters declaration			+++
+#echo +++++++++++++++++++++++++++++++++++++++++++++++++
+
 index_min=0.4
 index_max=1.1
 delta_index=0.1
-PWD=$(pwd)
+
+# mostrar la ruta del directorio actual
+#PWD=$(pwd)
 #echo $PWD
-# +++ Imprimo mensaje
-#echo #
-#echo ++++++++++++++++++ START ++++++++++++++++++
-#echo #
-# +++ Bucle for para crear un directorio diferente para cada valor del parámetro V_L
-## The C-style Bash for loop ##
-#for i in seq -f "%f" $index_min $delta_index $index_max
-for i in $(seq $index_min $delta_index $index_max);do
-  #echo $i
-  cd results-V_L-$i
-  #for q in 1..2
-    energ=($(awk 'NR==5,NR==25 {print $3}' veigen_X1))
-  #done
-  echo ${i} ${energ[@]}
-  cd ..
+
+#echo +++++++++++++++++++++++++++++++++++++++++++++++++
+#echo +++				START SCRIPT				+++
+#echo +++++++++++++++++++++++++++++++++++++++++++++++++
+
+for i in $(seq $index_min $delta_index $index_max)
+	do
+		cd results-V_L-$i
+		
+		# crear un array de elementos que hay dentro de un archivo
+		energy=($(awk 'NR==5,NR==24 {print $3}' veigen_X1))
+		
+		# escribir todos los elementos del array
+		# también funcionaría ${array[@]:s:n} que escribe n elementos contando desde el elemento s
+		echo ${i} ${energy[@]}
+		
+		cd ..
 done
+
+#echo +++++++++++++++++++++++++++++++++++++++++++++++++
+#echo +++				END SCRIPT				+++
+#echo +++++++++++++++++++++++++++++++++++++++++++++++++
