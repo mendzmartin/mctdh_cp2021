556bb46cb0c68db536f9d5cb01ebc0fa1d24de63
mendzmartin <chmartinmendez@gmail.com>
2021-12-15 10:23:03 -0300

    15/12/2021

--- double_quantum_dot_model/mctdh/results-V_L-1.0/script_veigen_X1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# take from 5-row to 10-row from xxxx file and save data in xxxx.dat
-# -F, tells awk to use , as the field separator on input. This is equivalent to but much briefer than BEGIN {FS = ","}.
-# NR := Number of Record
-# print $1,$3 tells awk to print the first and the third columns.
-
-awk -F, 'NR==5, NR==24 {print $1,$3}' veigen_X1 >> first_20_eigenvalues-V_L-1.0_.dat
