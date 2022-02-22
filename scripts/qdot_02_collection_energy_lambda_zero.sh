5cc094299d703f54b2039826e677dcb2b539097c
mendzmartin <chmartinmendez@gmail.com>
2022-02-22 20:38:29 -0300

    22/02/2022-20:38h

--- scripts/qdot_02_collection_energy_lambda_zero.sh
+++ scripts/qdot_02_collection_energy_lambda_zero.sh
@@ -11,7 +11,7 @@ delta_index=0.1
 cd ../double_quantum_dot_model/qdot_02
 
 # REMOVE EXISTING DATA
-rm -f result_fundamental_energy_lambda_zero.dat
+rm -f result_fundamental_energy_lambda_zero_07.dat
 
 for i in $(seq $index_min $delta_index $index_max)
 	do
@@ -20,7 +20,7 @@ for i in $(seq $index_min $delta_index $index_max)
 	rm -Rf double_qd_model_02
 	
 	# START EXECUTION
-	mctdh85 -w -mnd -p V_L $i,au -p lambda 0 input_file_02_convergence_anlysis_05.inp
+	mctdh85 -w -mnd -p V_L $i,au -p lambda 0 input_file_02_convergence_anlysis_07.inp
 
 
 	# START COLLECTION OF ENERGIES
@@ -35,6 +35,6 @@ for i in $(seq $index_min $delta_index $index_max)
 	
 	# WRITE DATA
 	cd ..
-	echo $result >> result_fundamental_energy_lambda_zero_05.dat
+	echo $result >> result_fundamental_energy_lambda_zero_07.dat
 
 done
