b77cf11e50a2fc2e6aa5a9d4f9bd15d74d83f4c8
mendzmartin <chmartinmendez@gmail.com>
2022-04-21 20:20:16 -0300

    21/04/2022-20:20h

--- scripts/sge_qdot_02_collect_ergy_job_distribute.sh
+++ scripts/sge_qdot_02_collect_ergy_job_distribute.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-
-# ++++++++++++++++++++++++++++++++++++++++++++++++++++
-# ++		COMANDOS PARA CORRER EL PROCESO			++
-# ++				DESDE BASH SHELL				++
-# ++++++++++++++++++++++++++++++++++++++++++++++++++++                                 
-
+                            
 # defines the bahaviour otherwise toggling
 LANG=en_US
 
@@ -13,8 +8,7 @@ lambda_min=0.05
 lambda_max=0.1
 delta_lambda=0.05
 COUNTER=1
-# numero de configuración (cambiar!)
-num_conf=01
+num_conf=01 # numero de configuración (cambiar!)
 
 # EXECUTION
 cd ../double_quantum_dot_model/qdot_02/energies_vs_lambda/study_of_performance/
@@ -25,26 +19,25 @@ cd ../../../../scripts/
 for i in $(seq $lambda_min $delta_lambda $lambda_max)
 	do
 		# change values outside according to looped variables
-		sed '67,120 s/${i}/'${i}'/' sge_qdot_02_collect_ergy_job_run.sh > intermediate_file_01_${COUNTER}.txt &&
-		sed '67,120 s/${num_conf}/'${num_conf}'/' intermediate_file_01_${COUNTER}.txt > intermediate_file_02_${COUNTER}.txt &&
-		sed '67,120 s/${COUNTER}/'${COUNTER}'/' intermediate_file_02_${COUNTER}.txt > sge_qdot_02_collect_ergy_job_run_${COUNTER}.sh &&
-		rm -f intermediate_file_01_${COUNTER}.txt intermediate_file_02_${COUNTER}.txt &&
-		cd ../double_quantum_dot_model/qdot_02/energies_vs_lambda/study_of_performance/ &&
-		sed '2 s/${COUNTER}/'${COUNTER}'/' input_file.inp > input_file_${COUNTER}.inp &&
-		cd ../../../../scripts/ &&
+		sed '67,120 s/${i}/'${i}'/g' sge_qdot_02_collect_ergy_job_run.sh > intermediate_file_01_${COUNTER}.txt
+		sed '67,120 s/${num_conf}/g'${num_conf}'/' intermediate_file_01_${COUNTER}.txt > intermediate_file_02_${COUNTER}.txt
+		sed '67,120 s/${COUNTER}/g'${COUNTER}'/' intermediate_file_02_${COUNTER}.txt > sge_qdot_02_collect_ergy_job_run_${COUNTER}.sh
+		rm -f intermediate_file_01_${COUNTER}.txt intermediate_file_02_${COUNTER}.txt
+		cd ../double_quantum_dot_model/qdot_02/energies_vs_lambda/study_of_performance/
+		sed '2 s/${COUNTER}/g'${COUNTER}'/' input_file.inp > input_file_${COUNTER}.inp
+		cd ../../../../scripts/
 		
 		# remove unnecessary data, give permission and run script
-		chmod +x sge_qdot_02_collect_ergy_job_run_${COUNTER}.sh &&
-		qsub ./sge_qdot_02_collect_ergy_job_run_${COUNTER}.sh && 
-		#rm -f sge_qdot_02_collect_ergy_job_run_${COUNTER}.sh &&
-		#rm -f ../double_quantum_dot_model/qdot_02/energies_vs_lambda/study_of_performance/input_file_${COUNTER}.inp &&
+		chmod +x sge_qdot_02_collect_ergy_job_run_${COUNTER}.sh
+		qsub ./sge_qdot_02_collect_ergy_job_run_${COUNTER}.sh 
+		rm -f sge_qdot_02_collect_ergy_job_run_${COUNTER}.sh
 		COUNTER=$((COUNTER+1))
 	done
 
-# Notes:
+	cd ../double_quantum_dot_model/qdot_02/energies_vs_lambda/study_of_performance/
+	rm -f input_file_${COUNTER}.inp
+	mv result_energy_vs_lambda.dat configuration_${num_conf}/result_energy_vs_lambda.dat
+	cd ../../../../scripts/
 
-# compilation:
-# 	1) chmod +x script.sh
-# 	2) ./script.sh
-# performance
-# 	1) perf stat -e cpu-clock,cpu-cycles,instructions,cache-references,cache-misses
+# Notes:
+# perf stat -e cpu-clock,cpu-cycles,instructions,cache-references,cache-misses
