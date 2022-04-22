c665283a818d2a989177d1a1b70ab47c4bd7222c
mendzmartin <chmartinmendez@gmail.com>
2022-04-22 13:11:41 -0300

    22/04/2022-13:11h

--- scripts/sge_qdot_02_collect_ergy_job_distribute.sh
+++ scripts/sge_qdot_02_collect_ergy_job_distribute.sh
@@ -8,7 +8,7 @@ lambda_min=0.05
 lambda_max=1
 delta_lambda=0.05
 COUNTER=1
-num_conf=01 # numero de configuración (cambiar!)
+num_conf=03 # numero de configuración (cambiar!)
 
 # EXECUTION
 cd ../double_quantum_dot_model/qdot_02/energies_vs_lambda/study_of_performance/
@@ -19,9 +19,9 @@ cd ../../../../scripts/
 for i in $(seq $lambda_min $delta_lambda $lambda_max)
 	do
 		# change values outside according to looped variables
-		sed '67,120 s/${i}/'${i}'/g' sge_qdot_02_collect_ergy_job_run.sh > intermediate_file_01_${COUNTER}.txt
-		sed '67,120 s/${num_conf}/'${num_conf}'/g' intermediate_file_01_${COUNTER}.txt > intermediate_file_02_${COUNTER}.txt
-		sed '67,120 s/${COUNTER}/'${COUNTER}'/g' intermediate_file_02_${COUNTER}.txt > sge_qdot_02_collect_ergy_job_run_${COUNTER}.sh
+		sed 's/${i}/'${i}'/g' sge_qdot_02_collect_ergy_job_run.sh > intermediate_file_01_${COUNTER}.txt
+		sed 's/${num_conf}/'${num_conf}'/g' intermediate_file_01_${COUNTER}.txt > intermediate_file_02_${COUNTER}.txt
+		sed 's/${COUNTER}/'${COUNTER}'/g' intermediate_file_02_${COUNTER}.txt > sge_qdot_02_collect_ergy_job_run_${COUNTER}.sh
 		rm -f intermediate_file_01_${COUNTER}.txt intermediate_file_02_${COUNTER}.txt
 		cd ../double_quantum_dot_model/qdot_02/energies_vs_lambda/study_of_performance/
 		sed '2 s/${COUNTER}/'${COUNTER}'/g' input_file.inp > input_file_${COUNTER}.inp
