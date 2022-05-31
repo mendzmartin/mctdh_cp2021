eebccc32f6e4a32d308cf4438a30e857d263e97a
mendzmartin <chmartinmendez@gmail.com>
2022-05-31 13:47:41 -0300

    31/05/2022-13:47h

---
+++ scripts/qdot_02_collect_ergy_job_run_03.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo '++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
+echo '+++++         START-START-START-START-START          +++++'
+echo '++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
+ 
+cd ../double_quantum_dot_model/qdot_02/energies_vs_lambda/study_of_performance_03/pc_famaf_martin/
+    rm -Rf 01_result_double_qd_model_02/
+    mctdh85 -mnd -w -p V_L 0.9,au -p lambda 0.5 input_file.inp
+cd ../../../../../scripts/
+
+echo '++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
+echo '++++               END-END-END-END-END                ++++'
+echo '++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
